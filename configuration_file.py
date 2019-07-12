0d322c8b8786a965db43a1af39c67013aef38300
hakabar <dalonso.sa@gmail.com>
2019-07-12 09:51:01 -0700

    Add files via upload

---
+++ configuration_file.py
@@ -0,0 +1,15 @@
+"""
+Configuration file to be use with compare_images scripts.
+
+This files contains the information to be set by the user (as images folder path) to allow the scripts to run.
+
+"""
+
+#Path to the folder contaiint the images to compare
+imgFolderPath='./pictures/'
+
+
+#Control threshold to manage the amount of variation between mean values of different images
+threshold=1.0
+
+
